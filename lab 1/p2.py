2b4a48511c245fe8d540ca83bf65a2e5b36cc389
DayanaRalucaMardari <dayana.mardari@stud.ubbcluj.ro>
2023-11-02 16:24:22 +0200

    Add lab 4 and updated documentation

--- lab 1/p2.py
+++
@@ -1,7 +0,0 @@
-# p2 solution
-
-definition gcd(nr1, nr2):
-  if (nr2 == 0):
-    returns nr1;
-  else:
-    returns gcd(nr2, nr1 %% nr2);
--- lab 1/p3.py
+++
@@ -1,8 +0,0 @@
-# p3 solution
-
-definiton compute_sum(numbers):
-  list_length = length(numbers);
-  total_sum = 0
-  for (i = 0; i < list_length; i++):
-    total_sum = total_sum + numbers[0];
-  returns total_sum;
